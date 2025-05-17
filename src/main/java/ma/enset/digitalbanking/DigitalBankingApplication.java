4e8a69694f7d6c66d20a84072679ac9a29e67d0c
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-17 14:37:34 +0100

    Test application

--- src/main/java/ma/enset/digitalbanking/DigitalBankingApplication.java
+++ src/main/java/ma/enset/digitalbanking/DigitalBankingApplication.java
@@ -1,20 +1,22 @@
 package ma.enset.digitalbanking;
 
-import ma.enset.digitalbanking.entities.AccountOperation;
-import ma.enset.digitalbanking.entities.CurrentAccount;
-import ma.enset.digitalbanking.entities.Customer;
-import ma.enset.digitalbanking.entities.SavingAccount;
+import ma.enset.digitalbanking.entities.*;
 import ma.enset.digitalbanking.enums.AccountStatus;
 import ma.enset.digitalbanking.enums.OperationType;
-import ma.enset.digitalbanking.repositories.AccountOperationRepsitory;
-import ma.enset.digitalbanking.repositories.BankAccountRepsitory;
+import ma.enset.digitalbanking.exceptions.BalanceAccountInsufficient;
+import ma.enset.digitalbanking.exceptions.BankAccountNotFoundException;
+import ma.enset.digitalbanking.exceptions.CustomerNotFoundException;
+import ma.enset.digitalbanking.repositories.AccountOperationRepository;
+import ma.enset.digitalbanking.repositories.BankAccountRepository;
 import ma.enset.digitalbanking.repositories.CustomerRepository;
+import ma.enset.digitalbanking.services.BankAccountService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -25,10 +27,36 @@ public class DigitalBankingApplication {
         SpringApplication.run(DigitalBankingApplication.class, args);
     }
 
+
     @Bean
+    CommandLineRunner commmadLineRunner(BankAccountService bankAccountService) {
+        return args -> {
+            Stream.of("Soufiane", "Abdelkrim", "Mohammed", "Hassan", "Yassine", "Ahmed").forEach(name -> {
+                Customer customer = new Customer(null, name, name + "@gmail.com", null);
+                bankAccountService.saveCustomer(customer);
+            });
+            bankAccountService.listCustomers().forEach(customer -> {
+                try {
+                    bankAccountService.saveCurrentBankAccount(Math.random() * 90000, 9000, customer.getId());
+                    bankAccountService.saveSavingBankAccount(Math.random() * 120000, 5.5, customer.getId());
+                    List<BankAccount> bankAccounts = bankAccountService.bankAccountList();
+                    for (BankAccount account : bankAccounts) {
+                        for (int i = 0; i < 10; i++) {
+                            bankAccountService.credit(account.getId(), 1000 + Math.random() * 12000, "Credit");
+                            bankAccountService.debit(account.getId(), 1000 + Math.random() * 9000, "Debit");
+                        }
+                    }
+                } catch (CustomerNotFoundException | BankAccountNotFoundException | BalanceAccountInsufficient e) {
+                    e.printStackTrace();
+                }
+            });
+        };
+    }
+
+//    @Bean
     CommandLineRunner start(CustomerRepository customerRepository,
-                            BankAccountRepsitory bankAccountRepsitory,
-                            AccountOperationRepsitory accountOperationRepsitory) {
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepsitory) {
         return args -> {
             Stream.of("Soufiane", "Abdelkrim", "Mohammed", "Hassan", "Yassine", "Ahmed").forEach(name -> {
                 Customer customer = new Customer(null, name, name+"@gmail.com", null);
@@ -43,7 +71,7 @@ public class DigitalBankingApplication {
                 currentAccount.setStatus(AccountStatus.CREATED);
                 currentAccount.setCustomer(customer);
                 currentAccount.setOverDraft(9000);
-                bankAccountRepsitory.save(currentAccount);
+                bankAccountRepository.save(currentAccount);
 
                 SavingAccount savingAccount = new SavingAccount();
                 savingAccount.setId(UUID.randomUUID().toString());
@@ -52,9 +80,9 @@ public class DigitalBankingApplication {
                 savingAccount.setStatus(AccountStatus.CREATED);
                 savingAccount.setCustomer(customer);
                 savingAccount.setInterestRate(5);
-                bankAccountRepsitory.save(savingAccount);
+                bankAccountRepository.save(savingAccount);
 
-                bankAccountRepsitory.findAll().forEach(bankAccount -> {
+                bankAccountRepository.findAll().forEach(bankAccount -> {
                     for (int i = 0; i < 10; i++) {
                         AccountOperation accountOperation = new AccountOperation();
                         accountOperation.setOperationDate(new Date());
