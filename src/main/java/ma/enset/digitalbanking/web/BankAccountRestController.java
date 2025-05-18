68c00e6cbce57200a4f3212fb47ceaeb33b76517
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-18 19:34:58 +0100

    Allow HTTP requests

--- src/main/java/ma/enset/digitalbanking/web/BankAccountRestController.java
+++ src/main/java/ma/enset/digitalbanking/web/BankAccountRestController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class BankAccountRestController {
     private BankAccountService bankAccountService;
 
